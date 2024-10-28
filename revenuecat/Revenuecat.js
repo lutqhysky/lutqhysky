aef4bee1265fd2a37c39bfbe4c9485a96237ce08
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-10-28 23:24:08 +0800

    更新 Revenuecat.js

--- revenuecat/Revenuecat.js
+++ revenuecat/Revenuecat.js
@@ -305,7 +305,7 @@ const list = {
   'CPUMonitor': { name: 'Pro', id: 'com.mars.cpumonitor_removeAd', cm: 'sjb' },  //手机硬件管家
   'totowallet': { name: 'all', id: 'com.ziheng.totowallet.onetimepurchase', cm: 'sjb' },  //图图记账
   '1Blocker': { name: 'premium', id: 'blocker.ios.iap.lifetime', cm: 'sjb' },  //1Blocker-广告拦截
-  'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' },  //VSCO-照片与视频编辑
+  'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' }  //VSCO-照片与视频编辑
   
 };
 
