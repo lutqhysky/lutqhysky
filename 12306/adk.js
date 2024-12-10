028bb7a889df5202f100b7e1344ebf3bbef2db9a
lutqhysky <79614893+lutqhysky@users.noreply.github.com>
2024-12-10 13:53:36 +0800

    Create adk.js

---
+++ 12306/adk.js
@@ -0,0 +1,18 @@
+let body = "";
+let obj = JSON.parse($request.body);
+const isQuanX = typeof $task !== "undefined";
+
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+if (isQuanX) {
+  $done({ body });
+} else {
+  $done({ response: { body } });
+}
